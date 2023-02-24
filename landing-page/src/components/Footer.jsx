266b8da188b7a38edda9fd713b396c068e693002
Patrick Goulding <patrickgoulding@gmail.com>
2023-02-23 17:40:24 -0800

    creatred basic footer

--- landing-page/src/components/Footer.jsx
+++ landing-page/src/components/Footer.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
-
+import {Box, Typography, Link} from '@mui/material';
 
 const Footer = () => {
     return (
-        <div>
-            <h2>
-                Footer
-            </h2>
-        </div>
+        <footer style={{backgroundColor:'#444444', color:'#fefefe', padding:'10px'}}>
+        <Typography align='center'>Serving the Denver Metro Area since 2017</Typography>
+        <Box sx={{mt:5, mb:2}}>
+            <Typography>Contact info:</Typography>
+                <Typography>3600 E 40th Ave</Typography>
+                <Typography>Denver, CO 80205</Typography>
+                <Typography><a href="mailto:info@energyscapes.com" color='#fefefe'>info@energyscapes.com</a></Typography>
+                <Typography><a href="tel:720-556-0759">720-556-0759</a></Typography>
+        </Box>
+        </footer>
     )
 }
 
