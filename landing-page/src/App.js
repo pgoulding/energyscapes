948a13b19bade798af526828dd8a9d687a531cd5
Patrick Goulding <patrickgoulding@gmail.com>
2023-02-23 17:37:09 -0800

    change logo size

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -10,11 +10,10 @@ import Footer from './components/Footer';
 function App() {
   return (
     <main className="App">
-
       <AppBar component="nav" sx={{bgcolor:"#5b6236"}}>
         <Toolbar style={{display:'flex', justifyContent:'space-between', alignContent:'center', height:"100px"}}>
-          <img src={eslogo} style={{height:80, margin:"10px"}}/>
-          <Typography variant='h6'>Call or Text (720) 556-0759</Typography>
+          <img src={eslogo} style={{height:80, margin:10}}/>
+          <Typography variant='h6'>Call or Text <a href="tel:+17205560759" style={{color:'white', textDecoration:'none'}}>(720) 556-0759</a></Typography>
         </Toolbar>
       </AppBar>
         <ProductHero/>
